7a41cc7d30c0ccc27abb4de40642257b1d0dd973
Ilia Kulakov <Ilia_Kulakov@epam.com>
2025-07-05 21:12:58 +0200

    whisper: Implement JWT Authentication

--- whispering-mongo-db/server.js
+++ whispering-mongo-db/server.js
@@ -1,68 +1,116 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import { getAll, getById, create, updateById, deleteById } from './store.js';
+import * as user from './stores/user.js';
+import * as whisper from './stores/whisper.js';
+import { generateToken, requireAuthentication } from './utils.js';
 
 const app = express();
-
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.set('view engine', 'ejs');
 
 app.get('/about', async (req, res) => {
-  const whispers = await getAll();
+  const whispers = await whisper.getAll();
   res.render('about', { whispers });
 });
 
-app.get('/api/v1/whisper', async (req, res) => {
-  const whispers = await getAll();
-  res.json(whispers);
+app.get('/logout', (req, res) => {
+  res.redirect('/login');
 });
 
-app.get('/api/v1/whisper/:id', async (req, res) => {
+app.get('/login', async (req, res) => {
+  res.render('login');
+});
+
+app.post('/login', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const foundUser = await user.getUserByCredentials(username, password);
+    const accessToken = generateToken({ username, id: foundUser._id });
+    res.json({ accessToken });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+app.get('/signup', async (req, res) => {
+  res.render('signup');
+});
+
+app.post('/signup', async (req, res) => {
+  try {
+    const { username, password, email } = req.body;
+    const newUser = await user.create(username, password, email);
+    const accessToken = generateToken({ username, id: newUser._id });
+    res.json({ accessToken });
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+  }
+});
+
+app.get('/api/v1/whisper', requireAuthentication, async (req, res) => {
+  const storedWhispers = await whisper.getAll();
+  res.json(storedWhispers);
+});
+
+app.get('/api/v1/whisper/:id', requireAuthentication, async (req, res) => {
   const id = req.params.id;
-  const whisper = await getById(id);
-  if (!whisper) {
+  const storedWhisper = await whisper.getById(id);
+  if (!storedWhisper) {
     res.sendStatus(404);
   } else {
-    res.json(whisper);
+    res.json(storedWhisper);
   }
 });
 
-app.post('/api/v1/whisper', async (req, res) => {
+app.post('/api/v1/whisper', requireAuthentication, async (req, res) => {
   const { message } = req.body;
+  const user = req.user;
   if (!message) {
     res.sendStatus(400);
   } else {
-    const whisper = await create(message);
-    res.status(201).json(whisper);
+    const storedWhisper = await whisper.create(message, user.id);
+    res.status(201).json(storedWhisper);
   }
 });
 
-app.put('/api/v1/whisper/:id', async (req, res) => {
+app.put('/api/v1/whisper/:id', requireAuthentication, async (req, res) => {
   const id = req.params.id;
   const { message } = req.body;
 
   if (!message) {
     res.sendStatus(400);
   } else {
-    const whisper = await getById(id);
-    if (!whisper) {
+    const storedWhisper = await whisper.getById(id);
+
+    if (!storedWhisper) {
       res.sendStatus(404);
-    } else {
-      await updateById(id, message);
-      res.sendStatus(200);
+      return;
     }
+
+    if (storedWhisper.author.id !== req.user.id) {
+      res.sendStatus(403);
+      return;
+    }
+
+    await whisper.updateById(id, message);
+    res.sendStatus(200);
   }
 });
 
-app.delete('/api/v1/whisper/:id', async (req, res) => {
+app.delete('/api/v1/whisper/:id', requireAuthentication, async (req, res) => {
   const id = req.params.id;
-  const whisper = await getById(id);
-  if (!whisper) {
+  const user = req.user;
+  const storedWhisper = await whisper.getById(id);
+  if (!storedWhisper) {
     res.sendStatus(404);
     return;
   }
-  await deleteById(id);
+  if (storedWhisper.author.id !== user.id) {
+    res.sendStatus(403);
+    return;
+  }
+  await whisper.deleteById(id);
   res.sendStatus(200);
 });
 
